dbee217a433a9e0cad9190e0cc41a61e73fe6395
Ivan Ivanov <ipivanov11@gmail.com>
2017-01-30 20:09:28 +0200

    Added md-button comp

---
+++ src/app/shared/md-button/md-button.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'button-overview-example',
+    templateUrl: './md-button.component.html',
+})
+export class ButtonOverviewExample {
+}
