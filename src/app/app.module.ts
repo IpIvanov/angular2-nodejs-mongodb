d62d09c59d1aa5bab84aaab84eaf1ad121c72bf6
Ivan Ivanov <ipivanov11@gmail.com>
2017-01-30 19:14:50 +0200

    Added angular2 material
    added angular material: https://material.angular.io/
    and other configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { routing } from './app.router';
-import { effects, store, instrumentation } from './store';
-import { SharedModule } from './shared/shared.module';
-import { WeatherService } from './weather/weather.service';
+import {AppComponent} from './app.component';
+import {routing} from './app.router';
+import {effects, store, instrumentation} from './store';
+import {SharedModule} from './shared/shared.module';
+import {WeatherService} from './weather/weather.service';
+
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    FormsModule,
-    HttpModule,
-    store,
-    effects,
-    routing,
-    instrumentation
-  ],
-  providers: [
-    WeatherService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        FormsModule,
+        HttpModule,
+        store,
+        effects,
+        routing,
+        instrumentation,
+        MaterialModule.forRoot()
+    ],
+    providers: [
+        WeatherService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,45 +1,54 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { ModalComponent, ModalDirectivesDirective } from './modal/modal.component';
-import { CommonModule } from '@angular/common';
-import { TopNavigationComponent } from './top-navigation/top-navigation.component';
-import { RouterModule } from '@angular/router';
-import { SubNavigationComponent } from './sub-navigation/sub-navigation.component';
-import { NotesComponent } from './notes/notes.component';
-import { ButtonComponent } from './button/button.component';
-import { InputComponent } from './input/input.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CardComponent } from './card/card.component';
-import { LoaderComponent } from './loader/loader.component';
+import {ModalComponent, ModalDirectivesDirective} from './modal/modal.component';
+import {CommonModule} from '@angular/common';
+import {TopNavigationComponent} from './top-navigation/top-navigation.component';
+import {RouterModule} from '@angular/router';
+import {SubNavigationComponent} from './sub-navigation/sub-navigation.component';
+import {NotesComponent} from './notes/notes.component';
+import {ButtonComponent} from './button/button.component';
+import {InputComponent} from './input/input.component';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {CardComponent} from './card/card.component';
+import {LoaderComponent} from './loader/loader.component';
+
+import {ButtonOverviewExample} from './md-button/md-button.component';
+
+import {MaterialModule} from '@angular/material';
+
 
 @NgModule({
-  declarations: [
-    ModalComponent,
-    TopNavigationComponent,
-    SubNavigationComponent,
-    ModalDirectivesDirective,
-    CardComponent,
-    ButtonComponent,
-    LoaderComponent,
-    InputComponent,
-    NotesComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  exports: [
-    ModalComponent,
-    ModalDirectivesDirective,
-    TopNavigationComponent,
-    LoaderComponent,
-    CardComponent,
-    ButtonComponent,
-    InputComponent,
-    SubNavigationComponent,
-    NotesComponent
-  ]
+    declarations: [
+        ModalComponent,
+        TopNavigationComponent,
+        SubNavigationComponent,
+        ModalDirectivesDirective,
+        CardComponent,
+        ButtonComponent,
+        LoaderComponent,
+        InputComponent,
+        NotesComponent,
+        ButtonOverviewExample
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule
+    ],
+    exports: [
+        ModalComponent,
+        ModalDirectivesDirective,
+        TopNavigationComponent,
+        LoaderComponent,
+        CardComponent,
+        ButtonComponent,
+        InputComponent,
+        SubNavigationComponent,
+        NotesComponent,
+        ButtonOverviewExample
+    ]
 })
-export class SharedModule {}
+export class SharedModule {
+}
