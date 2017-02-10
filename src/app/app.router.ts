235950f3a2ec1436ab5fc0309f1302f06d7fb693
Ivan Ivanov <ipivanov11@gmail.com>
2017-02-10 17:24:57 +0200

    Better ui card

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,7 +4,7 @@ import { PreventLoggedInAccess } from './shared/user/control.access';
 
 const routes: Route[] = [
     { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-    { loadChildren: 'app/dashboard/dashboard.module#DashboardModule', path: 'dashboard', canActivate: [PreventLoggedInAccess] },
+    { loadChildren: 'app/dashboard/dashboard.module#DashboardModule', path: 'dashboard', canActivate: [PreventLoggedInAccess] }, //
     { loadChildren: 'app/profile/profile.module#ProfileModule', path: 'profile' },
     { loadChildren: 'app/login/login.module#LoginModule', path: 'login' },
     { loadChildren: 'app/signup/signup.module#SignupModule', path: 'signup' }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,8 +11,6 @@ import { Observable } from 'rxjs';
 })
 export class DashboardComponent implements OnInit {
   form: FormGroup;
-  homeTeamImgs: Array<string> = [];
-  awayTeamImgs: Array<string> = [];
   title: string;
   description: string;
   startDate = new Date().toISOString().slice(0, 10);
@@ -25,45 +23,18 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getTodaysMatches(this.startDate, this.tommorow);
+    this.getTodaysMatches(this.startDate, this.endDate);
   }
 
   getTodaysMatches(startDate, endDate): void {
     this.footballData.getLeagueFixturesByDay(startDate, endDate)
       .subscribe(
       res => {
+        console.log(res)
         this.todaysMatches = res.fixtures;
-        this.getHomeTeamsImgs(res.fixtures);
-        this.getAwayTeamsImgs(res.fixtures);
       },
       err => console.log(err),
       () => console.log('Stop loader fetching is complete!')
       )
-  }
-
-  // try to get images from http://api.football-data.org/v1/teams?name=Manchester%20United
-
-  getHomeTeamsImgs(teams) {
-    let homeImgs$ = [];
-    teams.forEach(team => {
-      homeImgs$.push(this.footballData.getTeam(team._links.homeTeam.href));
-    });
-    Observable.forkJoin(homeImgs$)
-      .subscribe((res: any) => {
-        console.log(res);
-        this.homeTeamImgs = res;
-      });
-  }
-
-  getAwayTeamsImgs(teams) {
-    let awayImgs$ = [];
-    teams.forEach(team => {
-      awayImgs$.push(this.footballData.getTeam(team._links.awayTeam.href));
-    });
-    Observable.forkJoin(awayImgs$)
-      .subscribe((res: any) => {
-        console.log(res);
-        this.awayTeamImgs = res;
-      });
   }
 }
