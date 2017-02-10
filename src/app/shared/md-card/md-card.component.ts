27692f4907099e9cce789fac95c956ea20597b8f
Ivan Ivanov <ipivanov11@gmail.com>
2017-02-10 22:12:52 +0200

    Moved ng2-facebook into deps object

--- src/app/shared/md-card/md-card.component.ts
+++ src/app/shared/md-card/md-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -6,7 +6,7 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
   templateUrl: './md-card.component.html',
   styleUrls: ['./md-card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   // @Input() homeTeamImgs: string;
   // @Input() awayTeamImgs: string;
   @Input() title: string;
@@ -14,4 +14,15 @@ export class CardComponent {
   @Input() odds: Object;
   @Input() status: string;
   @Input() result: Object;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (this.status === 'IN_PLAY') {
+      this.status = 'LIVE';
+    }
+    if (this.status === 'TIMED') {
+      this.status = 'Sched';
+    }
+  }
 }
