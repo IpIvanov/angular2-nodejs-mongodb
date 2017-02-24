3a897010ea0c2d6eccf6980ed64ffbf3eabe238d
Ivan Ivanov <ipivanov11@gmail.com>
2017-02-24 19:48:25 +0200

    Preparing user model for predictions field
    Also fixes for navigation component and login flow component.

--- server/models/user/model.ts
+++ server/models/user/model.ts
@@ -2,20 +2,36 @@ import * as mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
     local: {
-        email: String,
+        email: { type: String, unique: true },
         password: String,
         avatarImg: String,
         salt: String,
         token: String
     },
     facebook: {
-        id: String,
+        id: { type: String, unique: true },
         gender: String,
         token: String,
         email: String,
         name: String,
         avatarImg: String
     },
+    predictions: [{
+        awayTeamName: String,
+        homeTeamName: String,
+        date: String,
+        odds: {
+            awayWin: Number,
+            draw: Number,
+            homeWin: Number
+        },
+        result: {
+            goalsAwayTeam: Number,
+            goalsHomeTeam: Number
+        },
+        prediction: String,
+        winningPrediction: Boolean
+    }],
     created_at: {
         type: Date,
         default: Date.now
--- server/routes/register.ts
+++ server/routes/register.ts
@@ -34,10 +34,10 @@ registerRouter.post('/signup', function (request: Request, response: Response, n
 
 // login method
 registerRouter.post('/login', function (request: Request, response: Response, next: NextFunction) {
-    let userEmail = request.body.local.email;
+    let userEmail = request.body.localUser.email;
     User.find({ 'local.email': userEmail }, function (err, doc) {
         if (doc.length > 0 && (doc[0].local.email === userEmail)) {
-            pbkdf2(request.body.local.password, doc[0].local.salt, 10000, length, digest, (err: Error, hash: Buffer) => {
+            pbkdf2(request.body.localUser.password, doc[0].local.salt, 10000, length, digest, (err: Error, hash: Buffer) => {
                 if (err) {
                     response.json({ error: err, message: 'Error.' });
                 }
