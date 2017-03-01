1cc556389954cc92a95baf657ce46c63e005706b
Ivan Ivanov <ivan.ivanov@ontotext.com>
2017-03-01 17:03:41 +0200

    Added more animations for predictions comp

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { FootballDataService } from './../shared/football-data/football-data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -7,7 +7,19 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('toggleContainerWidth', [
+      state('predictionsClosed', style({
+        transform: 'translateX(0)'
+      })),
+      state('predictionsOpened', style({
+        transform: 'translateX(-280px)'
+      })),
+      transition('predictionsClosed => predictionsOpened', animate('200ms ease-in')),
+      transition('predictionsOpened => predictionsClosed', animate('200ms ease-out')),
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
   form: FormGroup;
@@ -20,12 +32,14 @@ export class DashboardComponent implements OnInit {
   tommorow = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().slice(0, 10);
   todaysMatches: Array<Object>;
   sideMenuState: string = 'predictionsClosed';
+  containerWidth: number;
 
-  constructor(public footballData: FootballDataService) {
+  constructor(public footballData: FootballDataService, private el: ElementRef) {
   }
 
   ngOnInit() {
     this.getTodaysMatches(this.startDate, this.endDate);
+    this.containerWidth = this.el.nativeElement.querySelector('.predictions-list').offsetWidth;
   }
 
   getTodaysMatches(startDate, endDate): void {
@@ -40,21 +54,35 @@ export class DashboardComponent implements OnInit {
   }
 
   handleAddPrediction(prediction) {
+    let removedElmIndex;
     this.fixtures.forEach((fixture: any, index: number) => {
       if (fixture.homeTeamName === prediction.homeTeamName && fixture.awayTeamName === prediction.awayTeamName) {
         this.fixtures.splice(index, 1);
+        removedElmIndex = index;
       }
-    })
+    });
     let copy = Object.assign({}, prediction);
-    this.fixtures.push(copy);
+    if (removedElmIndex !== undefined) {
+      this.fixtures.splice(removedElmIndex, 0, copy);
+    } else {
+      this.fixtures.push(copy);
+    }
+    this.setSideMenuState();
+  }
+
+  handleMenuClose(menuState) {
+    this.sideMenuState = menuState;
+  }
+
+  getContainerWidth(): number {
+    return this.containerWidth;
+  }
+
+  setSideMenuState() {
     if (this.fixtures.length > 0) {
       this.sideMenuState = 'predictionsOpened';
     } else {
       this.sideMenuState = 'predictionsClosed';
     }
-  }
-
-  handleMenuClose(menuState) {
-    this.sideMenuState = menuState;
   }
 }
--- src/app/shared/predictions/predictions.component.ts
+++ src/app/shared/predictions/predictions.component.ts
@@ -28,6 +28,16 @@ import {
             })),
             transition('predictionsClosed => predictionsOpened', animate('200ms ease-in')),
             transition('predictionsOpened => predictionsClosed', animate('200ms ease-out')),
+        ]),
+        trigger('togglePrediction', [
+            state('in', style({ transform: 'translateX(0)' })),
+            transition('void => *', [
+                animate(300, keyframes([
+                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+                    style({ opacity: 1, transform: 'translateX(15px)', offset: 0.3 }),
+                    style({ opacity: 1, transform: 'translateX(0)', offset: 1.0 })
+                ]))
+            ])
         ])
     ]
 })
